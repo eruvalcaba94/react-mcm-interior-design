7ae6e77706dbace7c003ecbb9d5466436ffa818b
eruvalcaba94 <eric.s.ruvalcaba@gmail.com>
2023-07-01 13:13:51 -0700

    add: CSS Stylings

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className=''>
-        
+    <div className='flex'>
+      <div className='flex flex-row'>
+        <h2 className='text-5xl'>Mid-Century Mindset</h2>
+        <ul>
+          <li>How It Started</li>
+          <li>Explore Our Options</li>
+          <li>How It Works</li>
+        </ul>
+        <ul>
+          <li>Get Started</li>
+          <li>Login</li>
+        </ul>
+      </div>        
     </div>
   )
 }
