f563787b6f2f24b7bf188388cf0f0d04777eb9b8
eruvalcaba94 <eric.s.ruvalcaba@gmail.com>
2023-07-05 16:04:43 -0700

    add: pricing & stylings

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import { Buttons } from './components/Buttons'
 import Hero from './assets/green_purple_flower.jpg'
 import LivingRoom from './assets/white_livingroom.jpg'
 import SunlitBedroom from './assets/sunlight_bedroom.jpg'
+import TVLivingroom from './assets/livingroom_tv.jpg'
+import Consulation from './assets/consultation.jpg'
+import Development from './assets/development.jpg'
+import InPerson from './assets/in-person.jpg'
 
 const App = () => {
   return (
@@ -13,32 +17,86 @@ const App = () => {
       <div className=' bg-black/50 h-screen'>
         <img src={Hero} alt="Mid-Century Living Room" className='absolute -z-10 object-cover w-full h-screen' />
         <div className='flex flex-col justify-center items-center text-secondary text-center w-full min-h-screen'>
-          <h2 className='font-display text-5xl mb-5'>Your Mid-Century Modern Oasis</h2>
-          <p className='font-display text-xl'>Transform Your Space with Timeless Elegance</p>
+          <h2 className='font-display text-5xl mb-5'>
+            Your Mid-Century Modern Oasis</h2>
+          <p className='font-display text-xl'>
+            Transform Your Space with Timeless Elegance</p>
         </div>
       </div>
-      <div className='flex flex-col justify-center items-center  my-10 mx-14 md:mx-40 lg:mx-64 lg:my-28'>
-        <p className='font-body text-2xl lg:text-xl pb-4 font-light'>Love the space you're in</p>
-        <h2 className='font-display text-4xl text-start lg:text-center font-bold'>Bring your dream home to life with one-on-one design help & hand-picked products tailored to your style, space, and budget.</h2>
+      <div className='bg-secondary flex flex-col justify-center items-center  py-10 px-14 md:px-40 lg:px-64 lg:py-28'>
+        <p className='font-body text-2xl lg:text-xl pb-4 font-light'>
+          Love the space you're in</p>
+        <h2 className='font-display text-4xl text-start lg:text-center font-bold'>
+          Bring your dream home to life with one-on-one design help & hand-picked products tailored to your style, space, and budget.</h2>
       </div>
       <div className='flex flex-col pt-10'>
-        <div className='flex flex-col-reverse lg:flex-row justify-center items-center lg:mb-20 lg:px-28 w-full'>
-          <div className='flex flex-col justify-center items-center w-auto mx-10 mt-5 mb-8 lg:mx-28 lg:px-18'>
-            <h2 className='font-display text-3xl mb-5'>Timeless Elegance</h2>
-            <p className='font-body text-center text-lg md:text-xl'>From iconic chairs to sleek coffee tables, we bring timeless elegance to your home.</p>  
+        <div className='flex flex-col-reverse lg:flex-row justify-center items-center w-full lg:gap-4 lg:w-auto lg:mb-20 lg:px-10'>
+          <div className='flex flex-col justify-center items-center w-auto mx-10 mt-5 mb-8'>
+            <h2 className='font-display md:text-center text-3xl mb-5'>
+              Timeless Elegance</h2>
+            <p className='font-body text-center text-lg md:text-xl'>
+              From iconic chairs to sleek coffee tables, we bring timeless elegance to your home.</p>  
           </div>
           <img src={LivingRoom} alt="White Living Room" className='object-cover rounded-lg px-2 lg:px-0 w-full h-auto lg:w-1/2' />
         </div>
-        <div className='flex flex-col-reverse lg:flex-row-reverse justify-center items-center lg:mb-20 lg:px-28 w-full'>
-          <div className='flex flex-col justify-center items-center w-auto mx-10 mt-5 mb-8 lg:mx-28 lg:px-18'>
-            <h2 className='font-display text-3xl mb-5'>Elevate Your Space</h2>
-            <p className='font-body text-center text-lg md:text-xl'>Discover the perfect harmony of form and function with our expertly curated collection of mid-century modern furniture.</p>  
+        <div className='flex flex-col-reverse lg:flex-row-reverse justify-center items-center w-full lg:gap-4 lg:w-auto lg:mb-20 lg:px-10'>
+          <div className='flex flex-col justify-center items-center w-auto pb-10' >
+            <h2 className='font-display text-3xl mb-5'>
+              Elevate Your Space</h2>
+            <p className='font-body text-center text-lg md:text-xl'>
+              Discover the perfect harmony of form and function with our expertly curated collection of mid-century modern furniture.</p>  
           </div>
           <img src={SunlitBedroom} alt="Sunlit Bedroom" className='object-cover rounded-lg px-2 lg:px-0 w-full h-auto lg:w-1/2' />
         </div>
       </div>
-      <div className='bg-accent'>
-        <h2>Pricing</h2>
+      <div className='bg-[#304561] flex flex-col justify-center items-center lg:flex-row lg:flex-wrap lg:justify-center lg:gap-10 px-3 md:px-48 lg:px-0 py-14'>
+        <div className='bg-background flex flex-col rounded-lg mb-10 lg:w-[365px]'>
+          <img src={Consulation} alt="Consultation Art" className='object-cover rounded-t-lg w-full h-auto' />
+          <h2 className='font-display text-3xl text-center pt-2 pb-3'>
+            Bronze Design Package</h2>
+          <h3 className='font-body font-extrabold text-2xl text-center pb-4 text-[#f55d42]'>
+            <span className='line-through font-normal pr-2 text-gray-400'>$99</span> 
+            $50</h3>
+          <p className='font-body font-light text-lg text-center lg:h-[160px] pb-5 px-6'>
+            Our experts will provide you with a comprehensive consultation to assess your space and understand your goals.</p>
+        </div>
+        <div className='bg-background flex flex-col rounded-lg mb-10 lg:w-[365px]'>
+          <img src={Development} alt="Consultation Art" className='object-cover rounded-t-lg w-full h-auto' />
+          <h2 className='font-display text-3xl text-center pt-2 pb-3'>
+            Silver Design Package</h2>
+          <h3 className='font-body font-extrabold text-2xl text-center pb-4 text-[#f55d42]'>
+            <span className='line-through font-normal pr-2 text-gray-400'>$199</span> 
+            $150</h3>
+          <p className='font-body font-light text-lg text-center lg:h-[160px] pb-5 px-6'>
+            Our team will help you source and select the perfect pieces to elevate your space, ensuring a cohesive and stylish result.</p>
+        </div>
+        <div className='bg-background flex flex-col rounded-lg mb-10 lg:w-[365px]'>
+          <img src={InPerson} alt="Consultation Art" className='object-cover rounded-t-lg w-full h-auto' />
+          <h2 className='font-display text-3xl text-center pt-2 pb-3'>
+            Gold Design Package</h2>
+          <h3 className='font-body font-extrabold text-2xl text-center pb-4 text-[#f55d42]'>
+            <span className='line-through font-normal pr-2 text-gray-400'>$299</span> 
+            $250</h3>
+          <p className='font-body font-light text-lg text-center pb-5 px-6'>
+            Our team will oversee the entire design process, from procurement to installation, ensuring a seamless execution of your mid-century modern vision.</p>
+        </div>
+      </div>
+      <div className='bg-secondary flex flex-col p-10 md:p-20'>
+        <h2 className='font-display text-center text-2xl'>
+          "These Interior Designers have absolutely nailed it!"</h2>
+        <p className='font-body font-light text-center text-lg'>
+          - A Really Happy Client</p>
+      </div>
+      <div className='bg-[#c0a6dd] mb-5'>
+        <div className='flex flex-col lg:flex-row lg:justify-center lg:items-center'>
+          <img src={TVLivingroom} alt="TV Living Room" className='object-cover w-full lg:w-1/2 h-auto' />
+          <div className='text-center p-10 md:m-5'>
+            <p className='font-body pb-3'>
+              Bring New Life Home</p>
+            <h2 className='font-display text-3xl lg:text-5xl'>
+              Shop furniture & decor curated by our interior designers</h2>
+          </div>
+        </div>
       </div>
 
       <Footer />
